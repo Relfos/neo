8c6761793621691506266cda9eb791d58110a336
Relfos <relfos@gmail.com>
2018-06-06 01:46:29 +0100

    Swarm interface refactor

---
+++ Bluzelle.NEO.Sharp/Core/ISwarm.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+
+namespace Bluzelle.NEO.Sharp.Core
+{
+    public interface ISwarm
+    {
+        Task<bool> Create(string uuid, string key, byte[] value);
+        Task<byte[]> Read(string uuid, string key);
+
+        Task<bool> Update(string uuid, string key, byte[] value);
+        Task<bool> Remove(string uuid, string key);
+    }
+}

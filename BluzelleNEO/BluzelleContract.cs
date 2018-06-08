ae1c3a61c8053a750d8115e16457ac9306ead492
Relfos <relfos@gmail.com>
2018-06-09 00:17:17 +0100

    Updated contract to use different approach

--- BluzelleNEO/BluzelleContract.cs
+++ BluzelleNEO/BluzelleContract.cs
@@ -12,20 +12,18 @@ namespace Bluzelle.NEO.Contract
 
         private static readonly byte[] request_owner_prefix = { (byte)'R', (byte)'E', (byte)'Q', (byte)'O' };
         private static readonly byte[] request_count_prefix = { (byte)'R', (byte)'E', (byte)'Q', (byte)'C' };
+        private static readonly byte[] request_key_prefix = { (byte)'R', (byte)'E', (byte)'Q', (byte)'K' };
+        private static readonly byte[] request_value_prefix = { (byte)'R', (byte)'E', (byte)'Q', (byte)'V' };
+        private static readonly byte[] request_result_prefix = { (byte)'R', (byte)'E', (byte)'Q', (byte)'R' };
 
-        [DisplayName("blz_create")]
-        public static event Action<byte[], byte[], byte[]> OnCreate;
+        private static readonly byte[] request_create = { (byte)'C' };
+        private static readonly byte[] request_read = { (byte)'R' };
+        private static readonly byte[] request_update = { (byte)'U' };
+        private static readonly byte[] request_delete = { (byte)'D' };
 
-        [DisplayName("blz_read")]
-        public static event Action<byte[], byte[]> OnRead;
+        private static readonly byte[] value_null = {0};
 
-        [DisplayName("blz_update")]
-        public static event Action<byte[], byte[], byte[]> OnUpdate;
-
-        [DisplayName("blz_delete")]
-        public static event Action<byte[], byte[]> OnDelete;
-
-        public static bool Main(string operation, object[] args)
+        public static object Main(string operation, object[] args)
         {
             if (Runtime.Trigger == TriggerType.Verification)
             {
@@ -75,101 +73,84 @@ namespace Bluzelle.NEO.Contract
                 if (operation == "push")
                 {
                     if (args.Length != 2) return false;
-                    byte[] key = (byte[])args[0];
+                    BigInteger id = (BigInteger)args[0];
                     byte[] value = (byte[])args[0];
-                    return API_Push(key, value);
+                    return API_Push(id, value);
                 }
 
-                if (operation == "pull")
+                if (operation == "check")
                 {
-                    if (args.Length != 2) return false;
-                    byte[] address = (byte[])args[0];
-                    byte[] key = (byte[])args[0];
-                    return API_Pull(address, key);
+                    if (args.Length != 1) return false;
+                    BigInteger id = (BigInteger)args[0];
+                    return API_Pull(id);
                 }
 
                 return false;
             }
         }
 
-        private static bool API_Create(byte[] scripthash, byte[] key, byte[] value)
+        private static BigInteger API_Create(byte[] scripthash, byte[] key, byte[] value)
         {
-            if (!Runtime.CheckWitness(scripthash))
-            {
-                return false;
-            }
-
-            OnCreate(scripthash, key, value);
-
-            return true;
+            return MakeRequest(scripthash, request_create, key, value);
         }
 
-        private static bool API_Read(byte[] scripthash, byte[] key)
+        private static BigInteger API_Read(byte[] scripthash, byte[] key)
         {
-            if (!Runtime.CheckWitness(scripthash))
-            {
-                return false;
-            }
-
-            MakeRequest(scripthash);
-            OnRead(scripthash, key);
-
-            return true;
+            return MakeRequest(scripthash, request_read, key, value_null);
         }
 
-        private static bool API_Update(byte[] scripthash, byte[] key, byte[] value)
+        private static BigInteger API_Update(byte[] scripthash, byte[] key, byte[] value)
         {
-            if (!Runtime.CheckWitness(scripthash))
-            {
-                return false;
-            }
-
-            OnUpdate(scripthash, key, value);
-
-            return true;
+            return MakeRequest(scripthash, request_update, key, value);
         }
 
-        private static bool API_Delete(byte[] scripthash, byte[] key)
+        private static BigInteger API_Delete(byte[] scripthash, byte[] key)
         {
-            if (!Runtime.CheckWitness(scripthash))
-            {
-                return false;
-            }
-
-            OnDelete(scripthash, key);
-
-            return true;
+            return MakeRequest(scripthash, request_delete, key, value_null);
         }
 
-        private static bool API_Push(byte[] key, byte[] value)
+        private static bool API_Push(BigInteger req_id, byte[] value)
         {
             if (!Runtime.CheckWitness(Admin_Address))
             {
                 return false;
             }
 
+            var req_result_key = request_key_prefix.Concat(req_id.AsByteArray());
+            Storage.Put(Storage.CurrentContext, req_result_key, value);
+
             return true;
         }
 
-        private static bool API_Pull(byte[] scripthash, byte[] key)
+        private static byte[] API_Pull(BigInteger req_id)
         {
-            if (!Runtime.CheckWitness(scripthash))
+            var req_result_key = request_key_prefix.Concat(req_id.AsByteArray());
+            return Storage.Get(Storage.CurrentContext, req_result_key);
+        }
+
+        private static BigInteger MakeRequest(byte[] scriptHash, byte[] operation, byte[] key, byte[] value)
+        {
+            if (!Runtime.CheckWitness(scriptHash))
             {
-                return false;
+                return 0;
             }
 
-            OnDelete(scripthash, key);
-
-            return true;
-        }
-
-        private static void MakeRequest(byte[] scriptHash)
-        {
             var req_count = Storage.Get(Storage.CurrentContext, request_count_prefix).AsBigInteger();
             req_count = req_count + 1;
 
-            var req_key = request_owner_prefix.Concat(req_count.AsByteArray());
-            Storage.Put(Storage.CurrentContext, req_key, scriptHash);
+            var req_owner_key = request_owner_prefix.Concat(req_count.AsByteArray());
+            Storage.Put(Storage.CurrentContext, req_owner_key, scriptHash);
+
+            var req_key_key = request_key_prefix.Concat(req_count.AsByteArray());
+            Storage.Put(Storage.CurrentContext, req_key_key, operation.Concat(key));
+
+            if (value.AsBigInteger() != 0)
+            {
+                var req_val_key = request_value_prefix.Concat(req_count.AsByteArray());
+                Storage.Put(Storage.CurrentContext, req_val_key, value);
+            }
+
+            return req_count;
         }
     }
 }

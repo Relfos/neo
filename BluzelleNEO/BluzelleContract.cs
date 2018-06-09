61f7de4a60a906262a314593f6770ed62d55cd66
Relfos <relfos@gmail.com>
2018-06-09 01:18:22 +0100

    Uuid is no longer part of the storage key in the contract, but a separate field now

--- BluzelleNEO/BluzelleContract.cs
+++ BluzelleNEO/BluzelleContract.cs
@@ -8,10 +8,11 @@ namespace Bluzelle.NEO.Contract
     {
         public static readonly byte[] Admin_Address = "AHKPx5dZYnwAweQUJQH3UefoswKm6beEz2".ToScriptHash();
 
-        private static readonly byte[] request_owner_prefix = { (byte)'R', (byte)'E', (byte)'Q', (byte)'O' };
         private static readonly byte[] request_count_prefix = { (byte)'R', (byte)'E', (byte)'Q', (byte)'C' };
+        private static readonly byte[] request_owner_prefix = { (byte)'R', (byte)'E', (byte)'Q', (byte)'O' };
         private static readonly byte[] request_key_prefix = { (byte)'R', (byte)'E', (byte)'Q', (byte)'K' };
         private static readonly byte[] request_value_prefix = { (byte)'R', (byte)'E', (byte)'Q', (byte)'V' };
+        private static readonly byte[] request_uuid_prefix = { (byte)'R', (byte)'E', (byte)'Q', (byte)'U' };
         private static readonly byte[] request_result_prefix = { (byte)'R', (byte)'E', (byte)'Q', (byte)'R' };
 
         private static readonly byte[] request_create = { (byte)'C' };
@@ -74,19 +75,17 @@ namespace Bluzelle.NEO.Contract
 
                 if (operation == "push")
                 {
-                    if (args.Length != 3) return false;
+                    if (args.Length != 2) return false;
                     BigInteger id = (BigInteger)args[0];
-                    byte[] uuid = (byte[])args[1];
-                    byte[] value = (byte[])args[2];
-                    return API_Push(id, uuid, value);
+                    byte[] value = (byte[])args[1];
+                    return API_Push(id, value);
                 }
 
                 if (operation == "check")
                 {
-                    if (args.Length != 2) return false;
+                    if (args.Length != 1) return false;
                     BigInteger id = (BigInteger)args[0];
-                    byte[] uuid = (byte[])args[1];
-                    return API_Pull(id, uuid);
+                    return API_Pull(id);
                 }
 
                 return false;
@@ -113,23 +112,23 @@ namespace Bluzelle.NEO.Contract
             return MakeRequest(scripthash, request_delete, uuid, key, value_null);
         }
 
-        private static bool API_Push(BigInteger req_id, byte[] uuid, byte[] value)
+        private static bool API_Push(BigInteger req_id, byte[] value)
         {
             if (!Runtime.CheckWitness(Admin_Address))
             {
                 return false;
             }
 
-            var base_key = uuid.Concat(req_id.AsByteArray());
+            var base_key = req_id.AsByteArray();
             var req_result_key = request_key_prefix.Concat(base_key);
             Storage.Put(Storage.CurrentContext, req_result_key, value);
 
             return true;
         }
 
-        private static byte[] API_Pull(BigInteger req_id, byte[] uuid)
+        private static byte[] API_Pull(BigInteger req_id)
         {
-            var base_key = uuid.Concat(req_id.AsByteArray());
+            var base_key = req_id.AsByteArray();
             var req_result_key = request_key_prefix.Concat(base_key);
             return Storage.Get(Storage.CurrentContext, req_result_key);
         }
@@ -159,14 +158,17 @@ namespace Bluzelle.NEO.Contract
             var req_count = Storage.Get(Storage.CurrentContext, request_count_prefix).AsBigInteger();
             req_count = req_count + 1;
 
-            var base_key = uiid.Concat(req_count.AsByteArray());
+            var base_key = req_count.AsByteArray();
 
             var req_owner_key = request_owner_prefix.Concat(base_key);
             Storage.Put(Storage.CurrentContext, req_owner_key, scriptHash);
 
             var req_key_key = request_key_prefix.Concat(base_key);
             Storage.Put(Storage.CurrentContext, req_key_key, operation.Concat(key));
-    
+
+            var req_uuid_key = request_key_prefix.Concat(base_key);
+            Storage.Put(Storage.CurrentContext, req_uuid_key, uiid);
+
             if (value.AsBigInteger() != 0)
             {
                 var req_val_key = request_value_prefix.Concat(base_key);

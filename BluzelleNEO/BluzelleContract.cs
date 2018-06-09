d4ee44d567013ce038335ca3a814a2437ac04cab
Relfos <relfos@gmail.com>
2018-06-09 01:05:27 +0100

    Upgraded contract to include uuids

--- BluzelleNEO/BluzelleContract.cs
+++ BluzelleNEO/BluzelleContract.cs
@@ -1,7 +1,5 @@
 ﻿using Neo.SmartContract.Framework;
 using Neo.SmartContract.Framework.Services.Neo;
-using System;
-using System.ComponentModel;
 using System.Numerics;
 
 namespace Bluzelle.NEO.Contract
@@ -38,99 +36,122 @@ namespace Bluzelle.NEO.Contract
             {
                 if (operation == "create")
                 {
-                    if (args.Length != 3) return false;
+                    if (args.Length != 4) return false;
                     byte[] address = (byte[])args[0];
-                    byte[] key = (byte[])args[0];
-                    byte[] value = (byte[])args[0];
-                    return API_Create(address, key, value);
+                    byte[] uuid = (byte[])args[1];
+                    byte[] key = (byte[])args[2];
+                    byte[] value = (byte[])args[3];
+                    return API_Create(address, uuid, key, value);
                 }
 
                 if (operation == "read")
                 {
-                    if (args.Length != 2) return false;
+                    if (args.Length != 3) return false;
                     byte[] address = (byte[])args[0];
-                    byte[] key = (byte[])args[0];
-                    return API_Read(address, key);
+                    byte[] uuid = (byte[])args[1];
+                    byte[] key = (byte[])args[2];
+                    return API_Read(address, uuid, key);
                 }
 
                 if (operation == "update")
                 {
-                    if (args.Length != 3) return false;
+                    if (args.Length != 4) return false;
                     byte[] address = (byte[])args[0];
-                    byte[] key = (byte[])args[0];
-                    byte[] value = (byte[])args[0];
-                    return API_Update(address, key, value);
+                    byte[] uuid = (byte[])args[1];
+                    byte[] key = (byte[])args[2];
+                    byte[] value = (byte[])args[3];
+                    return API_Update(address, uuid, key, value);
                 }
 
                 if (operation == "delete")
                 {
-                    if (args.Length != 2) return false;
+                    if (args.Length != 3) return false;
                     byte[] address = (byte[])args[0];
-                    byte[] key = (byte[])args[0];
-                    return API_Delete(address, key);
+                    byte[] uuid = (byte[])args[1];
+                    byte[] key = (byte[])args[2];
+                    return API_Delete(address, uuid, key);
                 }
 
                 if (operation == "push")
                 {
-                    if (args.Length != 2) return false;
+                    if (args.Length != 3) return false;
                     BigInteger id = (BigInteger)args[0];
-                    byte[] value = (byte[])args[0];
-                    return API_Push(id, value);
+                    byte[] uuid = (byte[])args[1];
+                    byte[] value = (byte[])args[2];
+                    return API_Push(id, uuid, value);
                 }
 
                 if (operation == "check")
                 {
-                    if (args.Length != 1) return false;
+                    if (args.Length != 2) return false;
                     BigInteger id = (BigInteger)args[0];
-                    return API_Pull(id);
+                    byte[] uuid = (byte[])args[1];
+                    return API_Pull(id, uuid);
                 }
 
                 return false;
             }
         }
 
-        private static BigInteger API_Create(byte[] scripthash, byte[] key, byte[] value)
+        private static BigInteger API_Create(byte[] scripthash, byte[] uuid, byte[] key, byte[] value)
         {
-            return MakeRequest(scripthash, request_create, key, value);
+            return MakeRequest(scripthash, request_create, uuid, key, value);
         }
 
-        private static BigInteger API_Read(byte[] scripthash, byte[] key)
+        private static BigInteger API_Read(byte[] scripthash, byte[] uuid, byte[] key)
         {
-            return MakeRequest(scripthash, request_read, key, value_null);
+            return MakeRequest(scripthash, request_read, uuid, key, value_null);
         }
 
-        private static BigInteger API_Update(byte[] scripthash, byte[] key, byte[] value)
+        private static BigInteger API_Update(byte[] scripthash, byte[] uuid, byte[] key, byte[] value)
         {
-            return MakeRequest(scripthash, request_update, key, value);
+            return MakeRequest(scripthash, request_update, uuid, key, value);
         }
 
-        private static BigInteger API_Delete(byte[] scripthash, byte[] key)
+        private static BigInteger API_Delete(byte[] scripthash, byte[] uuid, byte[] key)
         {
-            return MakeRequest(scripthash, request_delete, key, value_null);
+            return MakeRequest(scripthash, request_delete, uuid, key, value_null);
         }
 
-        private static bool API_Push(BigInteger req_id, byte[] value)
+        private static bool API_Push(BigInteger req_id, byte[] uuid, byte[] value)
         {
             if (!Runtime.CheckWitness(Admin_Address))
             {
                 return false;
             }
 
-            var req_result_key = request_key_prefix.Concat(req_id.AsByteArray());
+            var base_key = uuid.Concat(req_id.AsByteArray());
+            var req_result_key = request_key_prefix.Concat(base_key);
             Storage.Put(Storage.CurrentContext, req_result_key, value);
 
             return true;
         }
 
-        private static byte[] API_Pull(BigInteger req_id)
+        private static byte[] API_Pull(BigInteger req_id, byte[] uuid)
         {
-            var req_result_key = request_key_prefix.Concat(req_id.AsByteArray());
+            var base_key = uuid.Concat(req_id.AsByteArray());
+            var req_result_key = request_key_prefix.Concat(base_key);
             return Storage.Get(Storage.CurrentContext, req_result_key);
         }
 
-        private static BigInteger MakeRequest(byte[] scriptHash, byte[] operation, byte[] key, byte[] value)
+        private static BigInteger MakeRequest(byte[] scriptHash, byte[] operation, byte[] uiid, byte[] key, byte[] value)
         {
             if (!Runtime.CheckWitness(scriptHash))
+            {
+                return 0;
+            }
+
+            if (operation.Length != 1)
+            {
+                return 0;
+            }
+
+            if (uiid.Length <= 0)
+            {
+                return 0;
+            }
+
+            if (key.Length <= 0)
             {
                 return 0;
             }
@@ -138,15 +159,17 @@ namespace Bluzelle.NEO.Contract
             var req_count = Storage.Get(Storage.CurrentContext, request_count_prefix).AsBigInteger();
             req_count = req_count + 1;
 
-            var req_owner_key = request_owner_prefix.Concat(req_count.AsByteArray());
+            var base_key = uiid.Concat(req_count.AsByteArray());
+
+            var req_owner_key = request_owner_prefix.Concat(base_key);
             Storage.Put(Storage.CurrentContext, req_owner_key, scriptHash);
 
-            var req_key_key = request_key_prefix.Concat(req_count.AsByteArray());
+            var req_key_key = request_key_prefix.Concat(base_key);
             Storage.Put(Storage.CurrentContext, req_key_key, operation.Concat(key));
-
+    
             if (value.AsBigInteger() != 0)
             {
-                var req_val_key = request_value_prefix.Concat(req_count.AsByteArray());
+                var req_val_key = request_value_prefix.Concat(base_key);
                 Storage.Put(Storage.CurrentContext, req_val_key, value);
             }
 

f2e59fa0cb88259f7afdb8b29efd6d498140b354
Relfos <relfos@gmail.com>
2018-06-14 00:08:04 +0100

    Added Demo contract

---
+++ BluzelleDemo/DemoContract.cs
@@ -0,0 +1,15 @@
+﻿using Neo.SmartContract.Framework;
+using Neo.SmartContract.Framework.Services.Neo;
+using System;
+using System.Numerics;
+
+namespace BluzelleDemo
+{
+    public class DemoContract : SmartContract
+    {
+        public static void Main()
+        {
+            Storage.Put(Storage.CurrentContext, "Hello", "World");
+        }
+    }
+}
